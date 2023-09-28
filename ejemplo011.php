cba8538d9f2dab72622500ee3731c198a838fa05
francoibanezweb <francoibanez.dev@gmail.com>
2023-09-28 09:00:29 -0300

    feat: create ej 011

---
+++ ejemplo011.php
@@ -0,0 +1,13 @@
+<?php
+/* Comentarios */
+
+/* Declaración de constantes */ 
+
+define("PORCENTAJE",15); /* 15 no lleva comillas, porque quiero que sea un 
+                        valor entero, no texto */
+define("NOMBRE", "MP Ediciones");
+
+echo PORCENTAJE;
+echo NOMBRE;
+
+?>
