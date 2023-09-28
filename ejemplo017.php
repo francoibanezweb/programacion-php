8e92d2d7f6eff0da33ee1058996fcb16bcc00b60
francoibanezweb <francoibanez.dev@gmail.com>
2023-09-28 11:44:14 -0300

    feat: add ejemplo17.php

---
+++ ejemplo017.php
@@ -0,0 +1,15 @@
+<?php
+/* Asigno valores a las variables */
+$precio = 12.5;
+
+if(is_double($precio))
+{
+  echo "La variable Precio es Double<br>";
+}
+else
+{
+  echo "La variable Precio no es Double<br>";
+}
+
+
+?>
