9694badbd98071e17ce926eccb46bc1356d261c8
francoibanezweb <francoibanez.dev@gmail.com>
2023-09-28 11:40:44 -0300

    ref: add some introductory text

--- ejemplo012.php
+++ ejemplo012.php
@@ -4,6 +4,9 @@
 $articulo="Teléfono Motorola 3456";
 $precio=12;
 
+echo "<br>&emsp;La función <b>gettype()</b> nos permite obtener el tipo de una
+variable<br>";
+
 echo "<br>Artículo es una variable del Tipo: ".gettype($articulo);
 echo "<br>Precio es una variable del Tipo: ".gettype($precio);
 
