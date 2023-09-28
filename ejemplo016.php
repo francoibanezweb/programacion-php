0ddfa1129832be5518751c07db13db5f6a2b2217
francoibanezweb <francoibanez.dev@gmail.com>
2023-09-28 11:42:53 -0300

    ref: add some introductory text

--- ejemplo016.php
+++ ejemplo016.php
@@ -1,4 +1,10 @@
 <?php
+
+echo "<br>&emsp;La función <b>is_array()</b> nos permite averiguar si una<br> 
+variable es un array, devolviendo un valor <b>TRUE</b> si la<br> variable 
+pasada como parámetro es un array o <b>FALSE</b><br> si no es un array.
+<br><br>";
+
 /* Asigno valores a las variables */
 $precio = 12.5;
 $nombres[0]="Marcela";
