21ad962080af5ec124efc242d5945bff5be6e285
francoibanezweb <francoibanez.dev@gmail.com>
2023-09-28 11:41:39 -0300

    ref: add some introductory text

--- ejemplo013.php
+++ ejemplo013.php
@@ -2,6 +2,11 @@
 /* Asigno valores a las variables */
 $precio=12.5;
 
+echo "<br>&emsp;La función <b>settype()</b> nos permite asignarle el tipo de 
+  variable a una variable.<br>Si se logró la conversión con éxito devuelve
+<b>TRUE</b>, si no fue así <b>FALSE</b>.<br>";
+
+
 /* Muestro el tipo de variable */
 echo "<br>Precio $precio es una variable del Tipo: ".gettype($precio);
 
