d0018ab3097976649909908e368175c000626f8c
francoibanezweb <francoibanez.dev@gmail.com>
2023-09-28 09:00:20 -0300

    feat: create ej 010

---
+++ ejemplo010.php
@@ -0,0 +1,11 @@
+<?php
+  /* Obteniendo variables de entorno */ 
+  /* Busco la IP */
+
+echo "<br>IP: ".getenv("REMOTE_ADDR");
+
+/* Path raiz del documento */
+
+echo "<br>IP: ".getenv("DOCUMENT_ROOT");
+
+?>
