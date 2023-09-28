70dff0d7d30d1b98bdafb782b326f8dc9708f51b
francoibanezweb <francoibanez.dev@gmail.com>
2023-09-28 09:00:34 -0300

    feat: create ej 012

---
+++ ejemplo012.php
@@ -0,0 +1,10 @@
+<?php
+/* Asigno valores a las variables */
+
+$articulo="Teléfono Motorola 3456";
+$precio=12;
+
+echo "<br>Artículo es una variable del Tipo: ".gettype($articulo);
+echo "<br>Precio es una variable del Tipo: ".gettype($precio);
+
+?>
