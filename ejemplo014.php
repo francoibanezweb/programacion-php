ff5ec5de5707902857e2645f227c7f2f4821637e
francoibanezweb <francoibanez.dev@gmail.com>
2023-09-28 11:41:57 -0300

    ref: add some introductory text

--- ejemplo014.php
+++ ejemplo014.php
@@ -2,6 +2,10 @@
 /* Asigno valores a las variables */
 $precio = 12.5;
 
+echo "<br>&emsp;La función <b>settype()</b> nos permite asignarle el tipo de 
+  variable a una variable.<br>Si se logró la conversión con éxito devuelve
+<b>TRUE</b>, si no fue así <b>FALSE</b>.<br><br>";
+
 if(settype($precio, "integer"))
 {
   echo "Hemos convertido el precio exitosamente<br>";
