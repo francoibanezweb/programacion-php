8bde76336dca37f7e209c013aac0d9b7cbfa8484
francoibanezweb <francoibanez.dev@gmail.com>
2023-09-28 11:44:35 -0300

    feat: add ejemplo018.php

---
+++ ejemplo018.php
@@ -0,0 +1,14 @@
+<?php
+/* Asigno valores a las variables */
+$precio = 12.5;
+
+if(isset($precio))
+{
+  echo "La variable Precio esta definida<br>";
+}
+else
+{
+  echo "La variable Precio no esta definida<br>";
+}
+
+?>
