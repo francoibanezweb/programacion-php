50cfc9f34351cb1f9b7009e0d668266e97daa0c5
francoibanezweb <francoibanez.dev@gmail.com>
2023-09-28 11:40:12 -0300

    ref: add some introductory text

--- ejemplo011.php
+++ ejemplo011.php
@@ -7,7 +7,12 @@ define("PORCENTAJE",15); /* 15 no lleva comillas, porque quiero que sea un
                         valor entero, no texto */
 define("NOMBRE", "MP Ediciones");
 
+echo "<br>&emsp;La función <b>define()</b> se utiliza para declarar constantes,
+<br> devuelve <b>TRUE</b> en caso de éxito y <b>FALSE</b> si ocurre un error.
+<br><br>";
+
 echo PORCENTAJE;
+echo "<br>";
 echo NOMBRE;
 
 ?>
