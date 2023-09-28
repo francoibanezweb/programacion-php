d4f941229ff9f71eec17dcc505616dc89aafda7a
francoibanezweb <francoibanez.dev@gmail.com>
2023-09-28 09:14:43 -0300

    feat: create ej 16

---
+++ ejemplo016.php
@@ -0,0 +1,25 @@
+<?php
+/* Asigno valores a las variables */
+$precio = 12.5;
+$nombres[0]="Marcela";
+$nombre[1]="Daniel";
+$nombre[2]="Gustavo";
+
+if(is_array($precio))
+{
+  echo "La variable Precio es un array<br>";
+}
+else
+{
+  echo "La variable Precio no es un array<br>";
+}
+if(is_array($nombres))
+{
+  echo "La variable Nombres es un array<br>";
+}
+else
+{
+  echo "La variable Nombres no es un array<br>";
+}
+
+?>
