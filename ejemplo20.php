593189babaa159a1ab3c29def662af1949841702
francoibanezweb <francoibanez.dev@gmail.com>
2023-09-30 11:40:47 -0300

    feat: create ejemplo20.php

---
+++ ejemplo20.php
@@ -0,0 +1,31 @@
+<?php
+/* Uno de los operadores metematicos */
+
+/* Definiremos 2 numeros */
+
+$a = 5;
+$b = 3;
+echo "Los numeros son a: $a y b: $b<br>";
+
+/* Suma de dos numeros */
+$resultado = $a + $b;
+echo "La suma de $a + $b es: $resultado<br>";
+
+/* Resta de dos numeros */
+$resultado = $a - $b;
+echo "La resta $a - $b es: $resultado<br>";
+
+/* Multiplicacion */
+$resultado = $a * $b;
+echo "La multiplicación $a * $b es: $resultado<br>";
+
+/* Division */
+$resultado = $a / $b;
+echo "La división $a / $b es: $resultado<br>";
+
+/* Calculo el resto de la Division */
+$resultado = $a % $b;
+echo "El resto de la división $a / $b es: $resultado<br>";
+
+
+?>
