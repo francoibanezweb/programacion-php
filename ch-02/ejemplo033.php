252885be136945b6f68ffb060c412f294f58f024
francoibanezweb <francoibanez.dev@gmail.com>
2023-10-05 15:06:54 -0300

    feat: complete ejemplo033.php

---
+++ ch-02/ejemplo033.php
@@ -0,0 +1,12 @@
+<?php
+/* Utilizando end if */
+/* Definimos variables y asignamos valores */
+
+$nombre = "Franco";
+
+if($nombre == "Franco") :
+  echo "Hola <b>$nombre</b><br>";
+  echo "Espero que pases un buen rato en este sitio<br>";
+  endif;
+
+?>
