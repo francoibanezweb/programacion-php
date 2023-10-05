34cd56d425a3367e964b5842d786b5ad9b33a29d
francoibanezweb <francoibanez.dev@gmail.com>
2023-10-05 13:55:38 -0300

    feat: reorganize files into folders for each chapter, add ejemplo031.php

---
+++ ch-02/ejemplo031.php
@@ -0,0 +1,11 @@
+<?php
+/* Otras opciones para escribir un if */
+/* Definimos variables y asignamos valores */
+
+$numero = 5;
+
+if($numero == 3)
+  echo "El número Es tres";
+  echo "El número No es tres";
+
+?>
