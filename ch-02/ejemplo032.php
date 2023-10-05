cf33ef412994cc6c0eef3697a67b01242149fa15
francoibanezweb <francoibanez.dev@gmail.com>
2023-10-05 15:06:42 -0300

    feat: complete ejemplo032.php

---
+++ ch-02/ejemplo032.php
@@ -0,0 +1,20 @@
+<?php
+/* Utilizando elseif */
+/* Definimos variables y asignamos valores */
+
+$numero = 5;
+
+if($numero <= 4)
+{
+   echo "El número es menor a cuatro o es cuatro";
+}
+elseif ($numero >= 6)
+{
+  echo "El número es mayor a 6 o es 6";
+}
+else
+{
+  echo "El número es 5 ";
+}
+
+?>
